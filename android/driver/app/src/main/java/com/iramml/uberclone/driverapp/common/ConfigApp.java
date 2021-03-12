9c922cf9defc45eeb3f4143a8a3836fb248b1695
chuchochacin <67656866+chuchochacin@users.noreply.github.com>
2021-03-12 13:02:20 -0400

    Update ConfigApp.java

--- android/driver/app/src/main/java/com/iramml/uberclone/driverapp/common/ConfigApp.java
+++ android/driver/app/src/main/java/com/iramml/uberclone/driverapp/common/ConfigApp.java
@@ -5,6 +5,6 @@ public class ConfigApp {
     public static final double timeRate = 0.35;
     public static final double distanceRate = 1.75;
     public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAA8ccWD4:APA91bEJVxg-45Np9BJnfWOPRt4q2SNjU7TCE66NE_oqS-9PABty5uRA1lQQh1khd2fDqipPVND5MQMPO-DY4pN38LFnOkET_Ukinrrg62X3LOGI4f0qYzQTm8i9B-xoLAo2jLnlzkVl";
-    public static final String GOOGLE_API_KEY = "AIzaSyDfrhyZAZpsoVmz4NaVVJLZC91MINPGr9s";
+    public static final String GOOGLE_API_KEY = "AIzaSyAXWApzaA0rQ4apMun7vI5YuCkZgwDPyyE";
 
 }
